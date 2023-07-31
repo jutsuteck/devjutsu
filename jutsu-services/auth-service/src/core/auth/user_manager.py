3c0d397a99ed77d087c20e9cdc56e99e8cf4d73c
jutsuteck <imanuelfebie@devjutsu.io>
2023-07-31 08:39:20 +0200

    Added basic but needed password validations

--- jutsu-services/auth-service/src/core/auth/user_manager.py
+++ jutsu-services/auth-service/src/core/auth/user_manager.py
@@ -1,8 +1,9 @@
+import re
 import uuid
 
-from typing import Optional
+from typing import Optional, Union
 from fastapi import Depends, Request
-from fastapi_users import BaseUserManager, UUIDIDMixin
+from fastapi_users import BaseUserManager, InvalidPasswordException, UUIDIDMixin
 from fastapi_users.db import SQLAlchemyUserDatabase
 
 from src.core.dependencies.database.database_manager import get_user_db
@@ -12,6 +13,20 @@ from src.models.v1.users import Member
 class UserManager(UUIDIDMixin, BaseUserManager[Member, uuid.UUID]):
     reset_password_token_secret = "SECRET"
     verification_token_secret = "SECRET"
+
+    async def validate_password(
+            self, password: str,
+            user: Union[Member, uuid.UUID]) -> None:
+        # replace consecutive spaces with a single space
+        password = re.sub('\s+', ' ', password)
+
+        if not password.isprintable():
+            raise InvalidPasswordException(
+                reason="The password contains unprintable characters.")
+
+        if len(password) < 12:
+            raise InvalidPasswordException(
+                reason="Password should be atleast 12 characters")
 
     async def on_after_register(
             self,
